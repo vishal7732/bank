8cf3ba36cccfc3cc9cd689db92c010323f2ef3c9
vishal7732 <58137166+vishal7732@users.noreply.github.com>
2020-07-04 23:20:04 +0530

    Update settings.py

--- bank/settings.py
+++ bank/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '2gn7-@60s%8e6at@rw8=xg((d$&s^q!iw)@x=y2=l09+$c1_qg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
